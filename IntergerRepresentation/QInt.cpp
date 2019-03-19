ee90e35f0cb39ec6a5e01d1247ed1f7da092d833
Nhan Nguyen Thanh <38831840+SilverLynx99@users.noreply.github.com>
2019-03-19 14:21:56 +0700

    update 3

--- IntergerRepresentation/QInt.cpp
+++ IntergerRepresentation/QInt.cpp
@@ -79,6 +79,8 @@ bool *DecToBin(QInt x)
 {
 	// Allocate an array of bool (128B), every bool store a bit
 	bool *bitArray = new bool[128];
+	
+	// da sua
 	for (int i = 0; i < 127; i++)
 		bitArray[i] = false;
 
