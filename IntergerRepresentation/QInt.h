c627cb31e05405f88599317ea818a6090b7f2ef7
Lolicon <1712625@student.hcmus.edu.vn>
2019-03-22 14:05:46 +0700

    Qfloat

--- IntergerRepresentation/QInt.h
+++ IntergerRepresentation/QInt.h
@@ -37,11 +37,3 @@ bool *DecToBin(QInt x);
 QInt BinToDec(bool *bit);
 
 
-struct Qfloat
-{
-	// Đánh dấu bit 0 là bit đầu tiên của data[0] 
-	// đi từ trái qua phải
-	unsigned int Data[4] = { 0 };
-};
-
-void ScanQfloat(Qfloat &x);
